cfbfe4566c94d199398e3e742fae9e3edbaea255
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2021-09-23 21:52:20 -0500

    giti

--- config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv").config();
-const config = {
-  consumer_key: process.env.TWITTER_API_KEY,
-  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
-  access_token: process.env.TWITTER_API_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_API_ACCESS_TOKEN_SECRET,
-};
-
-module.exports = config;
